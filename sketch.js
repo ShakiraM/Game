9a1df2cc036180d30ef4982cf6d6bf69dc865f64
blacklotus45 <s.molet@umiami.edu>
2016-04-22 21:34:34 -0400

    it works!

--- sketch.js
+++ sketch.js
@@ -9,6 +9,8 @@ var enemies;
 var GRAVITY = 1;
 var JUMP = 20;
 var button;
+var plats;
+var ground;
 
 
 function preload() {
@@ -26,26 +28,25 @@ function setup() {
 
 }
 
-var plats;
-var ground;
-
 function createGround() {
   
   var xInt = 45;
   ground = new Group();
   
-  while(xInt < windowWidth) {
-    var newFloor = createSprite(xInt, windowHeight, 100, 100);
+//  while(xInt < windowWidth) {
+  for (var x = -width/2; x < width; x+= 80) {
+    var newFloor = createSprite(x, height, 100, 100);
     newFloor.addImage(gfloor);
-    newFloor.setCollider("rectangle", 0, 35, 100, 100);
+    newFloor.setCollider("rectangle", 0, 35, 100, 80);
     ground.add(newFloor);
      
-     xInt += 80;
+//     xInt += 80;
    }
 }
 
 function draw() {
-  image(scene, 0, 0, windowWidth, windowHeight);
+//background(255);
+ image(scene, camera.position.x - width/2, camera.position.y - height/2, windowWidth, windowHeight);
   
   if(gameState == WAITING) {
    
@@ -54,9 +55,9 @@ function draw() {
   else if(gameState == PLAYING_GAME) {
     button.remove();
     
-    camera.position.x = player.position.x + width/4
+    camera.position.x = player.position.x;
     
-    image(scene, 0, 0, windowWidth, windowHeight);
+   // image(scene, 0, 0, windowWidth, windowHeight);
     
     player.velocity.y += GRAVITY;
     if(player.collide(ground)){
@@ -66,6 +67,15 @@ function draw() {
     if(player.collide(plats)) {
       player.velocity.y = 0;
     }
+
+    for(var i = 0; i < ground.length; i++) {
+      var g = ground.get(i);
+      if (g.position.x < player.position.x - width/2 - 50) {
+        console.log("Off screen for position: " + i);
+        g.position.x = player.position.x + width/2 + 50;
+      }
+    }
+
     /*for (var i = 0; i < enemies.length; i++) {
       var enemy = enemies.get(i);
       enemy.attractionPoint(.2, player.position.x, player.position.y);
@@ -77,7 +87,7 @@ function draw() {
     
   }
   else if (gameState == GAME_OVER) {
-    image(scene, 0, 0, windowWidth, windowHeight);
+  //  image(scene, 0, 0, windowWidth, windowHeight);
     //text("GAME OVER", width/2, height/2);
     
     button.remove();
@@ -141,7 +151,7 @@ function startGame() {
     //CREATE THE PLAYER
     player = createSprite(100, 100, 100, 100);
     player.setCollider("rectangle", 0, 20, 20, 100)
-    player.addAnimation("idle", "Art/Player/idle_0.png", "Art/Player/idle_1.png", "Art/Player/idle_2.png", "Art/Player/idle_3.png");
+    player.addAnimation("idle", "Art/Player/idle_1.png");
     player.addAnimation("runningright", "Art/Player/run_0.png", "Art/Player/run_1.png", "Art/Player/run_2.png", "Art/Player/run_3.png", "Art/Player/run_4.png", "Art/Player/run_5.png");
     player.addAnimation("jumping", "Art/Player/jump_0.png");
     player.addAnimation("runningleft","Art/Player/run2_0.png", "Art/Player/run2_1.png", "Art/Player/run2_2.png", "Art/Player/run2_3.png", "Art/Player/run2_4.png", "Art/Player/run2_5.png");
@@ -149,7 +159,7 @@ function startGame() {
     plats = new Group();
     var plat = createSprite(player.position.x + 300, 680, 50, 50);
     plat.addImage(gfloor);
-    plat.setCollider("rectangle", 0, 20, 100, 70);
+    plat.setCollider("rectangle", 0, 20, 100, 50);
     plats.add(plat);
     //CREATE SOME ENEMIES
     enemies = new Group();
